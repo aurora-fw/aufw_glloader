4c5926a63a403837e14441ba273217c6743b1703
Luís Ferreira <lsferreira169@gmail.com>
2017-11-23 20:34:11 +0000

    Port glad to aufw-glloader

--- module.cmake
+++ module.cmake
@@ -23,3 +23,14 @@
 # ensure the GNU Lesser General Public License version 3 requirements
 # will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
 
+set(AURORAFW_GLLOADER_OUT_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+find_package(PythonInterp REQUIRED)
+
+set(AURORAFW_GLLOADER_PROFILE "" CACHE STRING "OpenGL profile")
+set(AURORAFW_GLLOADER_API "3.3" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
+set(AURORAFW_GLLOADER_GENERATOR "c" CACHE STRING "Language to generate the binding for")
+set(AURORAFW_GLLOADER_EXTENSIONS "" CACHE STRING "Path to extensions file or comma separated list of extensions, if missing all extensions are included")
+set(AURORAFW_GLLOADER_SPEC "gl" CACHE STRING "Name of the spec")
+set(AURORAFW_GLLOADER_NO_LOADER OFF CACHE BOOL "No loader")
+set(AURORAFW_GLLOADER_EXPORT ON CACHE BOOL "Set export variables for external project")
+set(AURORAFW_GLLOADER_INSTALL OFF CACHE BOOL "Generate installation target")
